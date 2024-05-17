6b125b929af04806b3afb7c565d00489ef83e0e3
Joy <kimjoydev@gmail.com>
2024-05-17 16:15:11 +0900

    Initial Commit

---
+++ sesacExercise_newTerm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesacExercise_newTerm
+//
+//  Created by Joy Kim on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
