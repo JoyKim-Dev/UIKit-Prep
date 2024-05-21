befa9905f845fdab52bb972ca44b585966dfcbf6
Joy <kimjoydev@gmail.com>
2024-05-21 16:01:12 +0900

    Initial Commit

---
+++ sesacExercise_BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesacExercise_BMICalculator
+//
+//  Created by Joy Kim on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
