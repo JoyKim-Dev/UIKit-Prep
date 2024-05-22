443608481ffad012ffd8ce881bc1949eec53c3cf
Joy <kimjoydev@gmail.com>
2024-05-22 23:20:28 +0900

    Feat: UserDefaults 이용하여 저장 기능 추가, 리셋 기능 추가

--- sesacExercise_BMICalculator/BMIViewController.swift
+++ sesacExercise_BMICalculator/BMIViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class BMIViewController: UIViewController {
+class BMIViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
     
     @IBOutlet var firstQuestionLabel: UILabel!
     @IBOutlet var secondQuestionLabel: UILabel!
@@ -18,28 +19,42 @@ class BMIViewController: UIViewController {
     
     @IBOutlet var heightTextField: UITextField!
     @IBOutlet var weightTextField: UITextField!
+    @IBOutlet var nicknameTextField: UITextField!
     
     
     @IBOutlet var randomBMIBtn: UIButton!
     @IBOutlet var calcuateBMIBtn: UIButton!
+    @IBOutlet var resetBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        nicknameTextField.delegate = self
+        
+        nicknameTextField.text = UserDefaults.standard.string(forKey: "storedNickname")
+        heightTextField.text = UserDefaults.standard.string(forKey: "storedHeight")
+        weightTextField.text = UserDefaults.standard.string(forKey: "storedWeight")
+        
+        
         setUI()
-       
     }
     
     func setUI() {
         // 타이틀레이블, 서브레이블, 키 & 몸무게 텍스트필드, 랜덤버튼, 계산버튼 UI 구현 함수 실행
         designTitleLabelUI()
         designSubtitleLabelUI()
-        designBmiInfoTextfield(heightTextField, placeholder: "입력 단위는 kg입니다")
-        designBmiInfoTextfield(weightTextField, placeholder: "입력 단위는 cm입니다")
+        designBmiInfoTextfield(heightTextField, placeholder: "입력 단위는 cm입니다", keyboardType: .numberPad )
+        designBmiInfoTextfield(weightTextField, placeholder: "입력 단위는 kg입니다", keyboardType: .numberPad)
+        designBmiInfoTextfield(nicknameTextField, placeholder: "문자만 입력하세요.", keyboardType: .default)
         designRandomBmiBtnUI()
-        designCalculateBMIBtnUI()
+        designbasicBtnBMIBtnUI(calcuateBMIBtn, title: "결과확인")
+        designbasicBtnBMIBtnUI(resetBtn, title: "새로 검사해보기")
         firstQuestionLabel.text = "키가 어떻게 되시나요?"
         secondQuestionLabel.text = "몸무게는 어떻게 되시나요?"
+        nicknameLabel.text = "닉네임이 어떻게 되시나요?"
         
     }
     
@@ -52,13 +67,13 @@ class BMIViewController: UIViewController {
         subTitleLabel.numberOfLines = 2
         subTitleLabel.font = .systemFont(ofSize: 20)
     }
-    func designBmiInfoTextfield(_ l: UITextField, placeholder p : String) {
+    func designBmiInfoTextfield(_ l: UITextField, placeholder p : String, keyboardType k:UIKeyboardType) {
         
         l.font = .systemFont(ofSize: 20)
-        l.layer.cornerRadius = 20
+        l.layer.cornerRadius = 10
         l.clipsToBounds = true
         l.backgroundColor = .cyan // *backgroundColor 지정 안되는 오류 발생
-        l.keyboardType = .numberPad
+        l.keyboardType = k
         l.returnKeyType = .done
         l.textAlignment = .center
         l.borderStyle = .line
@@ -67,17 +82,31 @@ class BMIViewController: UIViewController {
         l.clearButtonMode = .whileEditing
         l.attributedPlaceholder = NSAttributedString(string: p, attributes: [.font: UIFont.systemFont(ofSize: 15, weight: .medium), NSAttributedString.Key.foregroundColor: UIColor.gray])
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        weightTextField.resignFirstResponder()
+        heightTextField.resignFirstResponder()
+        nicknameTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        weightTextField.resignFirstResponder()
+        heightTextField.resignFirstResponder()
+        nicknameTextField.resignFirstResponder()
+    }
+    
     func designRandomBmiBtnUI() {
         randomBMIBtn.setTitleColor(.red, for: .normal)
         
     }
-    func designCalculateBMIBtnUI() {
-        calcuateBMIBtn.backgroundColor = .purple
-        calcuateBMIBtn.setTitle("결과확인", for: .normal)
-        calcuateBMIBtn.setTitleColor(.white, for: .normal)
-        calcuateBMIBtn.layer.cornerRadius = 15
-        calcuateBMIBtn.clipsToBounds = true
-        calcuateBMIBtn.layer.borderColor = .none
+    func designbasicBtnBMIBtnUI(_ b:UIButton, title t:String) {
+        b.backgroundColor = .purple
+        b.setTitle(t, for: .normal)
+        b.setTitleColor(.white, for: .normal)
+        b.layer.cornerRadius = 15
+        b.clipsToBounds = true
+        b.layer.borderColor = .none
     }
     
     func alertSetUp(alertTitle t: String, alertMessage m: String) {
@@ -106,45 +135,78 @@ class BMIViewController: UIViewController {
          조건 3. 입력된 값의 숫자 범위 확인 (30보다 크고 200보다 작게) -> 범위 벗어날 경우 버튼 타이틀: "지나치게 작거나 큰 숫자를 입력하셨습니다."
          */
         
-        let height:String? = heightTextField.text
-        let weight:String? = weightTextField.text
+//        let height:String? = heightTextField.text
+//        let weight:String? = weightTextField.text
         
+        guard let height = heightTextField.text,
+              let weight = weightTextField.text,
+              height != "",
+              weight != "",
+              height.count < 4,
+              weight.count < 4 else {
+            
+            alertSetUp(alertTitle: "치명적오류", alertMessage: "입력값을 다시 확인하세요.")
+            return
+              }
         
-        if height != "" && weight != "" {
-            if let height = Double(height!.trimmingCharacters(in: .whitespaces)), let weight = Double(weight!.trimmingCharacters(in: .whitespaces)) {
-                if height > 0 && height < 300, weight > 0 && weight < 300 {
-                    let bmi = Int(round(weight / (height * height) * 10000))
-                    //calcuateBMIBtn.setTitle("BMI: \(bmi)", for: .normal)
-//                    let alert = UIAlertController(title: "BMI 결과", message: "BMI: \(bmi)", preferredStyle: .alert)
-//                    let cancel = UIAlertAction(title: "닫기", style: .cancel)
-//                    alert.addAction(cancel)
-//                    present(alert, animated: false)
-                    alertSetUp(alertTitle: "BMI결과가 나왔습니다!", alertMessage: "BMI: \(bmi)")
-                   
-                } else {
-                   // calcuateBMIBtn.setTitle("너무 작거나 큰 숫자를 입력했습니다.", for: .normal)
-//                    let alert = UIAlertController(title: "BMI 결과", message: "너무 작거나 큰 숫자를 입력했습니다.", preferredStyle: .alert)
-//                    let cancel = UIAlertAction(title: "닫기", style: .cancel)
-//                    alert.addAction(cancel)
-//                    present(alert, animated: false)
-                    alertSetUp(alertTitle: "에러발생!", alertMessage: "너무 작거나 큰 숫자를 입력했습니다.")
-                }
-            } else {
-//                  calcuateBMIBtn.setTitle("숫자를 입력해주세요.", for: .normal)
-//                let alert = UIAlertController(title: "BMI 결과", message: "숫자를 입력해주세요.", preferredStyle: .alert)
-//                let cancel = UIAlertAction(title: "닫기", style: .cancel)
-//                alert.addAction(cancel)
-//                present(alert, animated: false)
-                alertSetUp(alertTitle: "에러발생!", alertMessage: "숫자를 입력해주세요.")
-            }
-        } else {
-            //calcuateBMIBtn.setTitle("입력값이 없습니다.", for: .normal)
-//            let alert = UIAlertController(title: "BMI 결과", message: "입력값이 없습니다.", preferredStyle: .alert)
-//            let cancel = UIAlertAction(title: "닫기", style: .cancel)
-//            alert.addAction(cancel)
-//            present(alert, animated: false)
-            alertSetUp(alertTitle: "치명적오류", alertMessage: "입력값이 없습니다.")
+        guard let height = Double(height.trimmingCharacters(in: .whitespaces)),
+              let weight = Double(weight.trimmingCharacters(in: .whitespaces)) else {
+            
+            alertSetUp(alertTitle: "에러발생!", alertMessage: "숫자를 입력해주세요.")
+            return
         }
+        
+        let bmi = Int(round(weight / (height * height) * 10000))
+        alertSetUp(alertTitle: "BMI결과가 나왔습니다!", alertMessage: "BMI: \(bmi)")
+        
+        guard let nickname = nicknameTextField.text else {
+            print("닉네임은 nil")
+            return
+        }
+        
+       
+        
+        
+        UserDefaults.standard.set(height, forKey: "storedHeight")
+        UserDefaults.standard.set(weight, forKey: "storedWeight")
+        UserDefaults.standard.set(nickname, forKey: "storedNickname")
+        print("저장완료")
+        
+//        if height != "" && weight != "" {
+//            if let height = Double(height!.trimmingCharacters(in: .whitespaces)), let weight = Double(weight!.trimmingCharacters(in: .whitespaces)) {
+//                if height > 0 && height < 300, weight > 0 && weight < 300 {
+//                    let bmi = Int(round(weight / (height * height) * 10000))
+//                    //calcuateBMIBtn.setTitle("BMI: \(bmi)", for: .normal)
+////                    let alert = UIAlertController(title: "BMI 결과", message: "BMI: \(bmi)", preferredStyle: .alert)
+////                    let cancel = UIAlertAction(title: "닫기", style: .cancel)
+////                    alert.addAction(cancel)
+////                    present(alert, animated: false)
+//                    alertSetUp(alertTitle: "BMI결과가 나왔습니다!", alertMessage: "BMI: \(bmi)")
+//                   
+//                } else {
+//                   // calcuateBMIBtn.setTitle("너무 작거나 큰 숫자를 입력했습니다.", for: .normal)
+////                    let alert = UIAlertController(title: "BMI 결과", message: "너무 작거나 큰 숫자를 입력했습니다.", preferredStyle: .alert)
+////                    let cancel = UIAlertAction(title: "닫기", style: .cancel)
+////                    alert.addAction(cancel)
+////                    present(alert, animated: false)
+//                    alertSetUp(alertTitle: "에러발생!", alertMessage: "너무 작거나 큰 숫자를 입력했습니다.")
+//                }
+//            } else {
+////                  calcuateBMIBtn.setTitle("숫자를 입력해주세요.", for: .normal)
+////                let alert = UIAlertController(title: "BMI 결과", message: "숫자를 입력해주세요.", preferredStyle: .alert)
+////                let cancel = UIAlertAction(title: "닫기", style: .cancel)
+////                alert.addAction(cancel)
+////                present(alert, animated: false)
+//                alertSetUp(alertTitle: "에러발생!", alertMessage: "숫자를 입력해주세요.")
+//            }
+//        } else {
+//            //calcuateBMIBtn.setTitle("입력값이 없습니다.", for: .normal)
+////            let alert = UIAlertController(title: "BMI 결과", message: "입력값이 없습니다.", preferredStyle: .alert)
+////            let cancel = UIAlertAction(title: "닫기", style: .cancel)
+////            alert.addAction(cancel)
+////            present(alert, animated: false)
+//            alertSetUp(alertTitle: "치명적오류", alertMessage: "입력값이 없습니다.")
+//        }
         
         
         //        if let heightText = heightTextField.text, let weightText = weightTextField.text {
@@ -177,4 +239,20 @@ class BMIViewController: UIViewController {
         alertSetUp(alertTitle: "랜덤 BMI 결과입니다!", alertMessage: "BMI: \(bmi)")
         
     }
+    
+    @IBAction func resetBtnTapped(_ sender: UIButton) {
+        
+        UserDefaults.standard.removeObject(forKey: "storedHeight")
+        UserDefaults.standard.removeObject(forKey: "storedWeight")
+        UserDefaults.standard.removeObject(forKey: "storedNickname")
+        
+        nicknameTextField.text = UserDefaults.standard.string(forKey: "storedNickname")
+        heightTextField.text = UserDefaults.standard.string(forKey: "storedHeight")
+        weightTextField.text = UserDefaults.standard.string(forKey: "storedWeight")
+        print("삭제완료")
+        
+        
+    }
+    
+    
     }
