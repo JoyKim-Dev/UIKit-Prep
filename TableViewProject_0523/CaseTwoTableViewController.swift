f68ec6132557c2a3ff41e33fdedb8639a35733b9
Joy <kimjoydev@gmail.com>
2024-05-23 23:58:44 +0900

    Feat: tableview 코드로 구현

---
+++ TableViewProject_0523/CaseTwoTableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  CaseTwoTableViewController.swift
+//  TableViewProject_0523
+//
+//  Created by Joy Kim on 5/23/24.
+//
+
+import UIKit
+
+class CaseTwoTableViewController: UITableViewController {
+    // 전체설정(공지사항, 실험실, 버전 정보) / 개인 설정(개인/보안, 알림, 재팅, 열린프로필) / 기타(고객센터)
+    // section 3개, 셀 8개, 내용물
+    
+    let list = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "열린프로필"],["고객센터"]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .black
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return list.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        print(#function, indexPath)
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(#function, indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "setting")!
+        
+        cell.textLabel?.text = list[indexPath.section][indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+        cell.backgroundColor = .black
+        
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 1:
+            return "전체 설정"
+        case 2:
+            return "개인 설정"
+        case 3:
+            return "기타"
+        default:
+            return ""
+        }
+    }
+    
+}
+
+
+
+
+
+
+
