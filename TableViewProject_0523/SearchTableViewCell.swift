b4a078fb4c6295e970c5d65ccf3ceab670d004c0
Joy <kimjoydev@gmail.com>
2024-05-25 00:12:03 +0900

    Design: add Case3

---
+++ TableViewProject_0523/SearchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.swift
+//  TableViewProject_0523
+//
+//  Created by Joy Kim on 5/24/24.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
