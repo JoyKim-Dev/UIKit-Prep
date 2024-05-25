723fbdcd6039bd40bbcd96c9d5750669d8da98e8
Joy <kimjoydev@gmail.com>
2024-05-25 23:53:59 +0900

    Feat: 커스텀 테이블뷰 셀로 쇼핑리스트 구현

--- TableViewProject_0523/SearchTableViewCell.swift
+++ TableViewProject_0523/SearchTableViewCell.swift
@@ -7,17 +7,10 @@
 
 import UIKit
 
-class SearchTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    class SearchTableViewCell: UITableViewCell {
+        
+        @IBOutlet var checkBtn: UIButton!
+        @IBOutlet var favoriteBtn: UIButton!
+        @IBOutlet var titleLabel: UILabel!
+        
 }
